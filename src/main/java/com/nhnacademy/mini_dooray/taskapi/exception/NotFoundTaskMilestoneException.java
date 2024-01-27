a91dfa0e81de662a13176957f91acfa7fec0bff2
Imheroman <duddnd4457@naver.com>
2024-01-27 19:06:47 +0900

    [Refactoring, Feat] DB 수정 / TaskMilestone entity, service, repo upload / MilestoneRequestDto refactored

---
+++ src/main/java/com/nhnacademy/mini_dooray/taskapi/exception/NotFoundTaskMilestoneException.java
@@ -0,0 +1,7 @@
+package com.nhnacademy.mini_dooray.taskapi.exception;
+
+public class NotFoundTaskMilestoneException extends RuntimeException{
+    public NotFoundTaskMilestoneException(String message) {
+        super(message);
+    }
+}
